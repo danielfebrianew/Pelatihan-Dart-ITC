32d030be32f36b75f0fa921fed28efdbe169519a
Daniel Febrian <danielfebrian61@gmail.com>
2022-05-09 20:46:31 +0700

    Tugas 1

--- Tugas1/no_2.dart
+++ Tugas1/no_2.dart
@@ -3,7 +3,7 @@ void main(List<String> args) {
   var contoh = kalimat[2] + kalimat[3];
 
   var kata1 = kalimat[0] + kalimat[1] + kalimat[2] + kalimat[3] + kalimat[4] + kalimat[5] + kalimat[6] + kalimat[7];
-  var kata2 = kalimat[8] + kalimat[9] + kalimat[10] + kalimat[11] + kalimat[12] + kalimat[13] + kalimat[14];
+  var kata2 = kalimat.substring(8, 15);
 
   print("Contoh : " + contoh);
   print("Kata 1: " + kata1);
